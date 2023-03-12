c030102756b5f546240b21f41a738707c3a576c9
TheMarto <119800130+TheMarto@users.noreply.github.com>
2023-03-12 21:40:52 +0100

    Add files via upload

---
+++ calculadora simple.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace CALCULADORA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+
+            Console.WriteLine("SELECCIONE OPCION:" + "\n" + "1 PARA SUMA" + "\n" + "2 PARA RESTA" + "\n" + "3 PARA MULTIPLICACIÓN" + "\n" + "OTRO SALIR");
+
+            int x;
+            x = int.Parse(Console.ReadLine());
+
+            {
+                //valor de a y b
+                Console.WriteLine("SELECCIONE VALOR DE a");
+                a = int.Parse(Console.ReadLine());
+                Console.WriteLine("SELECCIONE VALOR DE b");
+                b = int.Parse(Console.ReadLine());
+                int suma = a + b;
+                int resta = a - b;
+                int multi = a * b;
+
+                //OPCION DE USUARIO
+                {
+                    switch (x)
+                    {
+                        case 1:
+                            
+                            Console.WriteLine("SUMA = " + suma);
+
+                            break;
+
+                        case 2:
+                            
+                            Console.Write("RESTA =" + resta);
+                            
+                            break;
+
+                        case 3:
+                            
+                            Console.Write("MULTIPLICACIÓN" + multi);
+                            
+                            break;                                                                                                            
+                    }
+
+                    Console.WriteLine("ADIOS...");
+                    Console.ReadKey();
+                }
+
+
+
+            }
+
+
+
+        }
+    }
+}
