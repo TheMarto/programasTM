465612e6ba633489db59a568b2678ddccc884bed
TheMarto <119800130+TheMarto@users.noreply.github.com>
2023-03-12 21:43:09 +0100

    Add files via upload

---
+++ PRUEBAS.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Example
+{
+    static void Main()
+    {
+        {
+            int i;
+            for ( i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+                if (i == 8)
+                {
+                    break;
+                }
+            }
+
+            
+                Console.WriteLine(i);
+            
+
+
+        }       
+        Console.ReadLine();
+    }
+
+}
