2ea1bb4586b99c25da2dae5820afbe9f997c9297
TheMarto <119800130+TheMarto@users.noreply.github.com>
2023-03-12 21:42:46 +0100

    Add files via upload

---
+++ PROGRAMA_VS.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace VariableDefinition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            do
+            {
+
+                Console.WriteLine("Introduce un número entre 1 y 9");
+                num = Int32.Parse(Console.ReadLine());
+
+            } while (num < 1 || num > 9);
+
+            for (int i = 1; i < 10; i++)
+
+                Console.WriteLine("{0} x {1} = {2}", num, i, num * i);
+
+        }
+    }
+}
